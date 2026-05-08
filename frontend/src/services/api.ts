30cec27407d210941dc924d24cbda1032b4afede
oleksandravolina <volinaoleksandra@gmail.com>
2026-05-08 13:29:30 +0200

    Update comment for token interceptor in api.ts
    Updated comment in api.ts to Polish.

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -9,7 +9,7 @@ const api = axios.create({
   },
 });
 
-// Interceptor для добавления токена
+// Interceptordo dodania tokenu
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
